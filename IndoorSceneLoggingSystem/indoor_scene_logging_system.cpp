72d04d6179ffbc7327574a3f4ecdfe85cd0390f6
ikegami takayuki <ikegami@CV.local>
2016-03-09 20:00:56 +0900

    KinectV2Clientの接続・送信・受信などの動作をIndoorSceneLoggingSystemのRun()関数内で行うように変更

--- IndoorSceneLoggingSystem/indoor_scene_logging_system.cpp
+++ IndoorSceneLoggingSystem/indoor_scene_logging_system.cpp
@@ -2,16 +2,25 @@
 
 IndoorSceneLoggingSystem::IndoorSceneLoggingSystem()
 {
-
+	Init();
 }
 void IndoorSceneLoggingSystem::Init()
 {
-	if ( !kinectv2_client_ )
+	if ( kinectv2_client_ )
 	{
-		kinectv2_client_ = new sensor_client::KinectV2Client();
+		delete kinectv2_client_;
 	}
+	kinectv2_client_ = new sensor_client::KinectV2Client();
 }
 void IndoorSceneLoggingSystem::Run()
 {
+	kinectv2_client_->Connect();
+	while ( 1 )
+	{
+		std::string send_message_string = "test";
+		kinectv2_client_->Send(send_message_string);
 
+		std::string receive_message_string;
+		kinectv2_client_->Receive( receive_message_string );
+	}
 }
--- IndoorSceneLoggingSystem/main.cpp
+++ IndoorSceneLoggingSystem/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
-#include "sensor_client\kinectv2_client.h"
+#include "indoor_scene_logging_system.h"
 // エントリーポイント
-void TestKinectV2Client()
-{
-	sensor_client::KinectV2Client kinectv2_client;
-	kinectv2_client.Connect();
-	while ( 1 )
-	{
-		std::string send_message_string = "test";
-		kinectv2_client.Send( send_message_string );
 
-		std::string receive_message_string;
-		kinectv2_client.Receive( receive_message_string );
-		std::cout << receive_message_string << std::endl;
-	}
-}
 int main()
 {
-	TestKinectV2Client();
+	IndoorSceneLoggingSystem indoor_scene_logging_system;
+	indoor_scene_logging_system.Run();
 	return 0;
 }
